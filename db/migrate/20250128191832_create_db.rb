5076bea10540561ab34da00e32e0536dc8e86e17
mutenesss <erickfraga0301@gmail.com>
2025-01-30 18:46:29 -0300

    Update migrate schema

--- db/migrate/20250128191832_create_db.rb
+++ db/migrate/20250128191832_create_db.rb
@@ -1,74 +1,74 @@
 class CreateDb < ActiveRecord::Migration[8.0]
   def change
-    create_table :users, primary_key: "UserID" do |t|
-      t.string "Matricula"
-      t.string "Email"
-      t.string "Nome"
-      t.string "Role"
-      t.string "Senha"      
+    create_table :users, primary_key: "UserId" do |t|
+      t.string :Matricula
+      t.string :Email
+      t.string :Nome
+      t.string :Role
+      t.string :Senha
       t.timestamps
     end
 
-    create_table :departamento, primary_key: "DepartamentoID" do |t|
-      t.string "Nome"
+    create_table :departamentos, primary_key: "DepartamentoId" do |t|
+      t.string :Nome
     end
 
-    create_table :template, primary_key: "TemplateID" do |t|
-      t.string "Nome"
-      t.string "PublicoAlvo"
-      t.string "Semestre" 
+    create_table :coordenadores, primary_key: "CoordenadorId" do |t|
+      t.references :departamento, foreign_key: { to_table: :departamentos }, index: true
+      t.references :user, foreign_key: { to_table: :users }, index: true
     end
 
-    create_table :coordenador, primary_key: "CoordenadorID" do |t|
-      t.belongs_to :departamento, foreign_key: true
-      t.belongs_to :user, foreign_key: true
+    create_table :materias, primary_key: "MateriaId" do |t|
+      t.string :Nome
+      t.string :Codigo
+      t.references :departamento, foreign_key: { to_table: :departamentos }, index: true
     end
 
-    create_table :materia, primary_key: "MateriaID" do |t|
-      t.string "Nome"
-      t.string "Codigo"
-      t.belongs_to :departamento, foreign_key: true
+    create_table :turmas, primary_key: "TurmaId" do |t|
+      t.string :Codigo
+      t.string :Semestre
+      t.references :materia, foreign_key: { to_table: :materias }, index: true
     end
 
-    create_table :turma, primary_key: "TurmaID" do |t|
-      t.string "Codigo"
-      t.string "Semestre"
-      t.belongs_to :materia, foreign_key: true
+    create_table :matriculas, primary_key: "MatriculaId" do |t|
+      t.references :user, foreign_key: { to_table: :users }, index: true
+      t.references :turma, foreign_key: { to_table: :turmas }, index: true
     end
 
-    create_table :matriculas, primary_key: "MatriculaID" do |t|
-      t.belongs_to :user, foreign_key: true
-      t.belongs_to :turma, foreign_key: true
+    create_table :templates, primary_key: "TemplateId" do |t|
+      t.string :Nome
+      t.string :PublicoAlvo
+      t.string :Semestre
     end
 
-    create_table :questionario, primary_key: "QuestionarioID" do |t|
-      t.string "Nome"
-      t.string "Turma"
-      t.belongs_to :template, foreign_key: true
+    create_table :questionarios, primary_key: "QuestionarioId" do |t|
+      t.string :Nome
+      t.string :Turma
+      t.references :template, foreign_key: { to_table: :templates }, index: true
     end
 
-    create_table :respondido, primary_key: "RespondidoID" do |t|
-      t.belongs_to :questionario, foreign_key: true
-      t.belongs_to :user, foreign_key: true
+    create_table :questaos, primary_key: "QuestaoId" do |t|
+      t.string :Nome
+      t.string :Texto
+      t.string :Tipo
+      t.references :template, foreign_key: { to_table: :templates }, index: true
     end
 
-    create_table :questao, primary_key: "QuestaoID" do |t|
-      t.string "Nome"
-      t.string "Texto"
-      t.string "Tipo"
-      t.belongs_to :template, foreign_key: true
+    create_table :questao_options, primary_key: "QuestaoOptionId" do |t|
+      t.string :Nome
+      t.string :Texto
+      t.references :questao, foreign_key: { to_table: :questaos }, index: true
     end
 
-    create_table :questaooption, primary_key: "QuestaoOptionID" do |t|
-      t.string "Nome"
-      t.string "Texto"
-      t.belongs_to :questao, foreign_key: true
+    create_table :respondidos, primary_key: "RespondidoId" do |t|
+      t.references :questionario, foreign_key: { to_table: :questionarios }, index: true
+      t.references :user, foreign_key: { to_table: :users }, index: true
     end
 
-    create_table :resposta, primary_key: "RespostaID" do |t|
-      t.string "Valor"
-      t.belongs_to :questao, foreign_key: true
-      t.belongs_to :respondido, foreign_key: true
+    create_table :respostas, primary_key: "RespostaId" do |t|
+      t.string :Valor
+      t.references :questao, foreign_key: { to_table: :questaos }, index: true
+      t.references :questionario, foreign_key: { to_table: :questionarios }, index: true
     end
   end
 end
